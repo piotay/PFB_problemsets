91d1fb4d1ff68ba6a39efbf2f3b1f40e5d689ed9
piotay <taylor.pio@emory.edu>
2024-10-15 11:46:06 -0400

    super fancy about me

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-print("My name: Taylor")
+import sys
+itsmyname = sys.argv[1]
+itsmycolor = sys.argv[2]
+itsmyactivity = sys.argv[3]
+itsmyanimal = sys.argv[4]
+
+print(f"My name: {itsmyname}\n"+f"My favorite color: {itsmycolor}\n"+f"My favorite activity: {itsmyactivity}\n"+f"My favorite animal: {itsmyanimal}")
 
